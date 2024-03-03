c1c2ce05f7a7a4785c59e97a18e1b6057ba4bdfc
Ëzo-Tanuki <michailbryanlie@gmail.com>
2024-03-03 21:34:32 +0700

    remove landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import Todo from "./pages/Todo";
-import Landing from "./pages/Landing";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Landing />}  /> */}
         <Route path="/" element={<Todo />} exact/>
       </Routes>
     </Router>
